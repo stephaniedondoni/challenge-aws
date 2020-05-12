88eefe85a6b953771593395ab089e5376b44d022
2104470427 <stephanie.dondoni@viavarejo.com.br>
2020-05-11 21:45:06 -0300

    Ajusted vpc, public subnet and nat-route files

--- modules/network/public.tf
+++ modules/network/public.tf
@@ -1,4 +1,4 @@
-  resource "aws_subnet" "public_subnet_1a" {
+resource "aws_subnet" "public_subnet_1a" {
     vpc_id = aws_vpc.cluster_vpc.id
 
     cidr_block = "10.0.0.0/20"
@@ -10,15 +10,9 @@
     tags = {
     "Name" = format("%s-public-1a", var.cluster_name)
     }
-  }
-
-resource "aws_route_table_association" "public_1a" {
-  subnet_id = aws_subnet.public_subnet_1a.id
-  route_table_id = aws_route_table_association.gw_route_table.id
-  
 }
 
-resource "aws_route_table_associate" "public_1a" {
-  subnet_id = aws_subnet.public_subnet_1a.id
-  route_table_id = aws_route_table.gw_route_table.id
+resource "aws_route_table_association" "public_1a" {
+    subnet_id = aws_subnet.public_subnet_1a.id
+    route_table_id = aws_route_table.gw_route_table.id
 }
